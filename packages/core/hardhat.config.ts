2774e10d027ed2ffefb509e1adbc884c62338943
vincenteof <14847208+vincenteof@users.noreply.github.com>
2024-05-05 23:46:45 +0800

    Initialize subgraph

--- packages/core/hardhat.config.ts
+++ packages/core/hardhat.config.ts
@@ -1,8 +1,10 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import 'hardhat-abi-exporter'
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  abiExporter: {}
 };
 
 export default config;

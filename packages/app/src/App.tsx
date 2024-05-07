33e4e887559745acc7c6c911b8d871c8a85bfe4e
vincenteof <14847208+vincenteof@users.noreply.github.com>
2024-05-07 23:10:12 +0800

    feat: basic integration for frontend

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useQuery, gql } from '@apollo/client'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { loading, data } = useQuery(gql`
+    query GetTransfers {
+      transfers {
+        id
+        from
+        to
+        value
+        blockNumber
+        blockTimestamp
+        transactionHash
+      }
+    }
+  `)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>My first Apollo app 😤</h2>
+      {loading ? (
+        'loading...'
+      ) : (
+        <ul>
+          {data?.transfers?.map((x) => (
+            <li key={x.id}>
+              from: {x.from}, to: {x.to}
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   )
 }
